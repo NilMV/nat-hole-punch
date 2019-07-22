ba700d1e753ac80daafd20449928210a810ada9b
nil.medvedev <nil.medvedev@vizor-games.com>
2019-07-22 18:12:35 +0300

    save

--- nat-udp1/nat-udp-client/main.go
+++ nat-udp1/nat-udp-client/main.go
@@ -67,59 +67,6 @@ func client(ctx context.Context, address string) (err error) {
 	return err
 }
 
-/*
-func client(ctx context.Context, address string) (err error) {
-	RemoteAddr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
-		return
-	}
-	conn, err := net.ListenUDP("udp", nil)
-
-	//	conn, err := net.DialUDP("udp", nil, RemoteAddr)
-	if err != nil {
-		return
-	}
-
-	log.Printf("Established connection to %s \n", address)
-	log.Printf("remote %s \n", RemoteAddr)
-
-	//log.Printf("Remote UDP address : %s \n", conn.RemoteAddr().String())
-	localHost := conn.LocalAddr()
-	publicHost := stunnedPublicIP()
-
-	log.Printf("Local UDP client address : %s \n", localHost)
-	log.Printf("Public UDP client address : %s \n", publicHost)
-
-	defer conn.Close()
-
-	// write a message to server
-	message := []byte("Hello UDP server!")
-
-	_, err = conn.WriteToUDP(message, RemoteAddr)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	buffer := make([]byte, 1024)
-	n, addr, err := conn.ReadFromUDP(buffer)
-
-	fmt.Println("UDP Server : ", addr)
-	fmt.Println("Received from UDP server : ", string(buffer[:n]))
-
-	peer, err := net.ResolveUDPAddr("udp", string(buffer[:n]))
-	if err != nil {
-		return
-	}
-	_, err = conn.WriteToUDP([]byte("Hello UDP peer!"), peer)
-	buffer = make([]byte, 1024)
-	n, addr, err = conn.ReadFromUDP(buffer)
-	fmt.Println("UDP Peer : ", addr)
-	fmt.Println("Received from UDP peer : ", string(buffer[:n]))
-
-	return err
-}
-*/
 var (
 	hostname = flag.String("host", "0.0.0.0", "host to listen to")
 	portnum  = flag.String("port", "6000", "port to listen to")
